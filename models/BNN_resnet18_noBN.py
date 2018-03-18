faf73542b3bc95d6f8fb7fde24d2fceea9923b91
Linghua Zeng <zenglh@outlook.com>
2018-03-18 10:07:13 +0800

    fix model

--- models/BNN_resnet18_noBN.py
+++ models/BNN_resnet18_noBN.py
@@ -7,51 +7,67 @@ model = Sequential([
     Residual([
         BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv2'),
         ], name='res2_1'),
-    ReLU(name='rrelu2'),
     Residual([
         BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv2'),
         ], name='res2_2'),
-    ReLU(name='rrelu2'),
+    Residual([
+        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res2_3'),
+    Residual([
+        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res2_4'),
     Residual([
         BinarizedSpatialConvolution(128,3,3,2,2, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv2'),
         ], connect=False, name='res3_1'),
-    ReLU(name='rrelu2'),
     Residual([
         BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv2'),
         ], name='res3_2'),
-    ReLU(name='rrelu2'),
+    Residual([
+        BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res3_3'),
+    Residual([
+        BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res3_4'),
     Residual([
         BinarizedSpatialConvolution(256,3,3,2,2, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv2'),
         ], connect=False, name='res4_1'),
-    ReLU(name='rrelu2'),
     Residual([
         BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv2'),
         ], name='res4_2'),
-    ReLU(name='rrelu2'),
+    Residual([
+        BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res4_3'),
+    Residual([
+        BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res4_4'),
     Residual([
         BinarizedSpatialConvolution(512,3,3,2,2, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv2'),
         ], connect=False, name='res5_1'),
-    ReLU(name='rrelu2'),
     Residual([
         BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
-        BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv2'),
         ], name='res5_2'),
-    ReLU(name='rrelu2'),
+    Residual([
+        BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res5_3'),
+    Residual([
+        BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
+        ReLU(name='rrelu1'),
+        ], name='res5_4'),
     SpatialAveragePooling(7,7,1,1, name='pool5'),
     Affine(1000, name='fc6')
 ])
