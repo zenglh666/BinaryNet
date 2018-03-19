6114181c7d73dabd5196185c957c4d0c945f0f80
Linghua Zeng <zenglh@outlook.com>
2018-03-19 10:54:24 +0800

    fix model

--- models/BNN_resnet18_noBN.py
+++ models/BNN_resnet18_noBN.py
@@ -5,67 +5,67 @@ model = Sequential([
     ReLU(name='relu1'),
     SpatialMaxPooling(3,3,2,2, name='pool1', padding='SAME'),
     Residual([
-        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res2_1'),
     Residual([
-        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res2_2'),
     Residual([
-        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res2_3'),
     Residual([
-        BinarizedSpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(64,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res2_4'),
     Residual([
-        BinarizedSpatialConvolution(128,3,3,2,2, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(128,3,3,2,2, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], connect=False, name='res3_1'),
     Residual([
-        BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res3_2'),
     Residual([
-        BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res3_3'),
     Residual([
-        BinarizedSpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(128,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res3_4'),
     Residual([
-        BinarizedSpatialConvolution(256,3,3,2,2, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(256,3,3,2,2, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], connect=False, name='res4_1'),
     Residual([
-        BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res4_2'),
     Residual([
-        BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res4_3'),
     Residual([
-        BinarizedSpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(256,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res4_4'),
     Residual([
-        BinarizedSpatialConvolution(512,3,3,2,2, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(512,3,3,2,2, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], connect=False, name='res5_1'),
     Residual([
-        BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res5_2'),
     Residual([
-        BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res5_3'),
     Residual([
-        BinarizedSpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
+        BinarizedWeightOnlySpatialConvolution(512,3,3, padding='SAME', name='rconv1'),
         ReLU(name='rrelu1'),
         ], name='res5_4'),
     SpatialAveragePooling(7,7,1,1, name='pool5'),
