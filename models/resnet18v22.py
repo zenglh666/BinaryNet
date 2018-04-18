f74b6f19024bc8dd85eaac974c98f94db029f038
Linghua Zeng <zenglh@outlook.com>
2018-04-18 12:00:53 +0800

    fix bug

--- models/resnet18v22.py
+++ models/resnet18v22.py
@@ -2,8 +2,6 @@ from nnUtils import *
 
 model = Sequential([
     SpatialConvolution(64,7,7,2,2, padding='SAME', name='conv1'),
-    BatchNormalization(name='batch1'),
-    ReLU(name='relu1'),
     SpatialMaxPooling(3,3,2,2, name='pool1', padding='SAME'),
     ResidualV2([
         BatchNormalization(name='rbatch1'),
@@ -61,6 +59,8 @@ model = Sequential([
         BatchNormalization(name='rbatch2'),
         ReLU(name='rrelu2'),
         SpatialConvolution(512,3,3, padding='SAME', name='rconv2'),], name='res5_2'),
+    BatchNormalization(name='batch5'),
+    ReLU(name='relu5'),
     SpatialAveragePooling(7,7,1,1, name='pool5'),
     Affine(1000, name='fc6')
 ])
